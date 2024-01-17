3c9681d44a88f7fd2fce78443f071bca22df5a41
Caden Christesen <cadenchristesen@gmail.com>
2024-01-17 10:03:24 -0600

    Initial Commit

---
+++ Slider/SliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderApp.swift
+//  Slider
+//
+//  Created by Caden Christesen on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct SliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
